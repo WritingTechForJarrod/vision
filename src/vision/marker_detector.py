55eef2764664d48b27b29a919cbfd70b8e12881f
maxlazarus <max.prokopenko@gmail.com>
2017-01-15 10:16:33 -0800

    Saving contour data in gen folder

--- src/vision/marker_detector.py
+++ src/vision/marker_detector.py
@@ -2,6 +2,7 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 import cv2
 import logging
 import numpy as np
+import time
 
 class ContrastFilter(object):
 	def __init__(self):
@@ -36,11 +37,6 @@ class ContourWrapper(object):
 		self._range = 10
 		self.updated = False
 		self.update(contour)
-		'''M = cv2.moments(contour)
-					if M['m00'] > 1:
-						centroid_x = int(M['m10']/M['m00'])
-						centroid_y = int(M['m01']/M['m00'])
-		'''
 
 	def is_same_contour(self,contour):
 		this_pos = self.last_position()
@@ -51,6 +47,12 @@ class ContourWrapper(object):
 	def last_position(self):
 		if len(self._history) > 0:
 			return self._history[-1][0]
+		else:
+			return None
+
+	def last_area(self):
+		if len(self._history) > 0:
+			return self._history[-1][1]
 		else:
 			return None
 
@@ -82,90 +84,82 @@ if __name__ == '__main__':
 	last_result = None
 	last_contours = []
 
-	while user_in != ord('x'):
-		global last_result
-		img = cv2.flip(get_image(),1)
-		#img = cv2.medianBlur(img,5)
-		#img = cv2.imread('../../img/dot_rg_low_light.png')
-		shape = img.shape
-		result = np.empty(shape,np.uint8)
-		final = np.empty(shape,np.uint8)
-		final.fill(0)
-		if len(shape) < 3:
-			raise ValueError
+	t0 = time.clock()
 
-		result = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-		result[:,:,2] = cv2.inRange(result[:,:,2],63,255)
-		#result[:,:,2] = cv2.inRange(result[:,:,1],64,255)
-		result = cv2.cvtColor(result,cv2.COLOR_HSV2BGR)
-		result = result[:,:,1]//2 - result[:,:,2]//2 + 127
-		
-		'''
-		kernel = np.zeros((9,1),np.float32)
-		kernel[0,0] = -0.0
-		kernel[1,0] = -0.3
-		kernel[2,0] = -0.3
-		kernel[3,0] = -0.3
-		kernel[4,0] = 0.0
-		kernel[5,0] = 0.3
-		kernel[6,0] = 0.3
-		kernel[7,0] = 0.3
-		kernel[8,0] = 0.0
-		'''
-		result = cv2.pyrDown(result)
-		#result = cv2.pyrDown(result)
-		k_array =  [[-3,-1,-1,-1,-3],
-					[-1, 1, 3, 1,-1],
-					[-1, 3, 6, 3,-1],
-					[-1, 1, 3, 1,-1],
-					[-3,-1,-1,-1,-3]]
-		kernel = np.array(k_array,np.float32)
-		final = cv2.filter2D(result,-1,kernel)
-		#final = cv2.medianBlur(final,3)
-		final = cv2.inRange(final,63,255)
-		'''
-		if last_result is not None:
-			real_final = last_result//2+final//2
-		else:
-			real_final = final
-		real_final = cv2.inRange(real_final,162,255)
-		'''
-		if 1:
-			# TODO make this less ugly-like. maybe just store default cv2 types and operate on them?
-			ret,contours,hierarchy = cv2.findContours(final,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
-			new_contours = [ContourWrapper(contour) for contour in contours]
+	shape = get_image().shape
+	result = np.empty(shape,np.uint8)
+	final = np.empty(shape,np.uint8)
+	final.fill(0)
 
-			for last_contour in last_contours:
-				last_contour.updated = False
-				for new_contour in contours:
-					if last_contour.is_same_contour(new_contour):
-						last_contour.update(new_contour)
+	with open('../../gen/'+time.strftime('%Y%m%d%H%M%S')+'.vcr','w') as f:
+		while user_in != ord('x'):
+			global last_result
+			img = cv2.flip(get_image(),1)
+			#img = cv2.medianBlur(img,5)
+			#img = cv2.imread('../../img/dot_rg_low_light.png')
+			
+			if len(shape) < 3:
+				raise ValueError
 
-			if len(last_contours) > 1:
-				circle_r = 7
-				circle_t = 2
-				circle_c = np.array([255,0,0])
+			result = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+			result[:,:,2] = cv2.inRange(result[:,:,2],63,255)
+			#result[:,:,2] = cv2.inRange(result[:,:,1],64,255)
+			result = cv2.cvtColor(result,cv2.COLOR_HSV2BGR)
+			result = result[:,:,1]//2 - result[:,:,2]//2 + 127
+			
+			result = cv2.pyrDown(result)
+			#result = cv2.pyrDown(result)
+			k_array =  [[-3,-1,-1,-1,-3],
+						[-1, 1, 3, 1,-1],
+						[-1, 3, 6, 3,-1],
+						[-1, 1, 3, 1,-1],
+						[-3,-1,-1,-1,-3]]
+			kernel = np.array(k_array,np.float32)
+			final = cv2.filter2D(result,-1,kernel)
+			#final = cv2.medianBlur(final,3)
+			final = cv2.inRange(final,63,255)
 
-				for contour in last_contours:
-					if contour.updated:
-						x,y = contour.last_position()
-						cv2.circle(img,(x,y),circle_r,circle_c,circle_t)
-						cv2.circle(result,(x,y),circle_r,circle_c,circle_t)
-						cv2.circle(final,(x,y),circle_r,circle_c,circle_t)
+			if 1:
+				# TODO make this less ugly-like. maybe just store default cv2 types and operate on them?
+				ret,contours,hierarchy = cv2.findContours(final,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+				new_contours = [ContourWrapper(contour) for contour in contours]
+				
+				new_contours[:] = [c for c in new_contours if c.last_area() > 1.0]
 
-			last_contours = new_contours
+				t = time.clock() - t0
+				f.write('t '+str(t)+'\n')
+				for contour in new_contours:
+					area = contour.last_area()
+					pos = contour.last_position()
+					if area is not None and pos is not None:
+						f.write(str(area)+' '+str(pos[0])+' '+str(pos[1])+'\n')
 
-			#px_pts = cv2.findNonZero(final)
-			'''
-			if (len(coords) > 1):
-				distance = ((coords[0][0]-coords[1][0])**2 + (coords[0][1]-coords[1][1])**2)**0.5
-				mainlog.debug(distance)
-	
-			'''
-		cv2.imshow(window_name,result)
-		last_result = final
-		user_in = cv2.waitKey(1)
+				if 1:
+					for last_contour in last_contours:
+						last_contour.updated = False
+						for new_contour in contours:
+							if last_contour.is_same_contour(new_contour):
+								last_contour.update(new_contour)
+								break
 
-	cv2.destroyAllWindows()
+					if len(last_contours) > 1:
+						circle_r = 7
+						circle_t = 2
+						circle_c = np.array([255,0,0])
+						
+						for contour in last_contours:
+							if contour.updated:
+								x,y = contour.last_position()
+								cv2.circle(img,(x,y),circle_r,circle_c,circle_t)
+								cv2.circle(result,(x,y),circle_r,circle_c,circle_t)
+								cv2.circle(final,(x,y),circle_r,circle_c,circle_t)
+
+				last_contours = new_contours
+
+			cv2.imshow(window_name,result)
+			last_result = final
+			user_in = cv2.waitKey(1)
+
+		cv2.destroyAllWindows()
 	
 

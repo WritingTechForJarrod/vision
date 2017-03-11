103313edcfd483be6d8e91c1b41b2e09029d0569
alexb <alexberlingeri@yahoo.com>
2017-03-11 11:34:41 -0800

    Eyetracker values scaled to screen size (0 to 1).

--- eye_tracker/EyeTrackerCommunication.c
+++ eye_tracker/EyeTrackerCommunication.c
@@ -1,6 +1,7 @@
 /*
- * This is an example that demonstrates how to connect to the EyeX Engine and subscribe to the lightly filtered gaze data stream.
+ * This code is a modified version of an the MinimalGazeDataStream example found in the Tobii EyeX SDK. Original header below.
  *
+ * This is an example that demonstrates how to connect to the EyeX Engine and subscribe to the lightly filtered gaze data stream.
  * Copyright 2013-2014 Tobii Technology AB. All rights reserved.
  */
 #define _WINSOCKAPI_
@@ -9,7 +10,7 @@
 #define SOCKET_SUB  "tcp://localhost:5556" // Eye tracker module listens to this socket for commands
 #define SOCKET_PUSH "tcp://localhost:5557" // Eye tracker module pushes updates on this socket
 #define TOPIC_FILTER "eyetracker"
-#define TIMEOUT_THRESHOLD 5 // Timeout before marco command does not reply with polo.
+#define TIMEOUT_THRESHOLD 10 // Timeout before marco command does not reply with polo.
 
 #include <Windows.h>
 #include <stdio.h>
@@ -25,13 +26,17 @@ static const TX_STRING InteractorId = "Twilight Sparkle";
 
 // global variables
 static TX_HANDLE g_hGlobalInteractorSnapshot = TX_EMPTY_HANDLE;
-static void *context;
-static void *subscription_socket;
-static void *push_socket;
-static int start_message_sent = 0;
-static int marco_blocking = 0;
-static int connection_successful = 0;
-static time_t last_data_sent_time;
+TX_CONTEXTHANDLE g_hContext = TX_EMPTY_HANDLE;
+static void *g_zmqcontext;
+static void *g_subscription_socket;
+static void *g_push_socket;
+static int g_start_message_sent = 0;
+static int g_marco_blocking = 0;
+static int g_connection_successful = 0;
+static time_t g_last_data_sent_time;
+static double g_screen_bound_x = 0;
+static double g_screen_bound_y = 0;
+static int g_screen_bounds_found = 0;
 
 /*
 * Array of valid messages eyetracker can send. Maintains one to one correspondence with "message_type" enumeration.
@@ -79,7 +84,7 @@ typedef enum command
 void send_zmq_message(ET_Message send_message) {
 	char buffer[100];
 	_snprintf(buffer, 50, "%s",eyetracker_messages[send_message]);
-	zmq_send(push_socket, buffer, strlen(buffer), 0);
+	zmq_send(g_push_socket, buffer, strlen(buffer), 0);
 }
 
 /*
@@ -117,6 +122,39 @@ void TX_CALLCONVENTION OnSnapshotCommitted(TX_CONSTHANDLE hAsyncData, TX_USERPAR
 }
 
 /*
+ * Handles a state-changed notification, or the response from a get-state operation.
+ */
+void OnStateReceived(TX_HANDLE hStateBag)
+{
+	TX_BOOL success;
+	TX_INTEGER eyeTrackingState;
+	TX_SIZE2 screenBounds;
+
+	success = (txGetStateValueAsInteger(hStateBag, TX_STATEPATH_EYETRACKINGSTATE, &eyeTrackingState) == TX_RESULT_OK);
+	success = (txGetStateValueAsSize2(hStateBag, TX_STATEPATH_EYETRACKINGSCREENBOUNDS, &screenBounds) == TX_RESULT_OK);
+	if (success) {
+		g_screen_bound_x = screenBounds.Width;
+		g_screen_bound_y = screenBounds.Height;
+		g_screen_bounds_found = 1;
+	}
+}
+
+/*
+ * Handles engine state change notifications.
+ */
+void TX_CALLCONVENTION OnEngineStateChanged(TX_CONSTHANDLE hAsyncData, TX_USERPARAM userParam)
+{
+	TX_RESULT result = TX_RESULT_UNKNOWN;
+	TX_HANDLE hStateBag = TX_EMPTY_HANDLE;
+
+	if (txGetAsyncDataResultCode(hAsyncData, &result) == TX_RESULT_OK && 
+		txGetAsyncDataContent(hAsyncData, &hStateBag) == TX_RESULT_OK) {
+		OnStateReceived(hStateBag);
+		txReleaseObject(&hStateBag);
+	}
+}
+
+/*
  * Callback function invoked when the status of the connection to the EyeX Engine has changed.
  */
 void TX_CALLCONVENTION OnEngineConnectionStateChanged(TX_CONNECTIONSTATE connectionState, TX_USERPARAM userParam)
@@ -134,23 +172,24 @@ void TX_CALLCONVENTION OnEngineConnectionStateChanged(TX_CONNECTIONSTATE connect
 		break;
 
 	case TX_CONNECTIONSTATE_DISCONNECTED:
-		//printf("The connection state is now DISCONNECTED (We are disconnected from the EyeX Engine)\n");
-		connection_successful = 0;
+		g_connection_successful = 0;
 		break;
 
 	case TX_CONNECTIONSTATE_TRYINGTOCONNECT:
-		//printf("The connection state is now TRYINGTOCONNECT (We are trying to connect to the EyeX Engine)\n");
-		connection_successful = 0;
+		g_connection_successful = 0;
 		break;
 
 	case TX_CONNECTIONSTATE_SERVERVERSIONTOOLOW:
+		g_connection_successful = 0;
 		send_zmq_message(ET_SERVER_LOW);
 		break;
 
 	case TX_CONNECTIONSTATE_SERVERVERSIONTOOHIGH:
+		g_connection_successful = 0;
 		send_zmq_message(ET_SERVER_HIGH);
 		break;
 	}
+	txGetStateAsync(g_hContext, TX_STATEPATH_EYETRACKING, OnEngineStateChanged, NULL);
 }
 
 /*
@@ -158,18 +197,21 @@ void TX_CALLCONVENTION OnEngineConnectionStateChanged(TX_CONNECTIONSTATE connect
  */
 void OnGazeDataEvent(TX_HANDLE hGazeDataBehavior)
 {
-	char send_message[30]; // Max message size is 30 bytes
+	char send_message[50]; // Max message size is 50 bytes
+	float x_coord;
+	float y_coord;
 	TX_GAZEPOINTDATAEVENTPARAMS eventParams;
-	DWORD screen_width = GetSystemMetrics(SM_CXSCREEN);
-	DWORD screen_height = GetSystemMetrics(SM_CYSCREEN);
-	float scale_factor_x = 1920.0/screen_width;
-	float scale_factor_y = 1080.0/screen_height;
 	if (txGetGazePointDataEventParams(hGazeDataBehavior, &eventParams) == TX_RESULT_OK) {
-		connection_successful = 1;
-		last_data_sent_time = time(NULL);
-		_snprintf(send_message, 50, "%s gaze %d,%d",TOPIC_FILTER, (int)(eventParams.X/scale_factor_x), (int)(eventParams.Y/scale_factor_y));
-		if (start_message_sent == 1 && marco_blocking == 0)
-			zmq_send(push_socket, send_message, strlen(send_message), 0);
+		g_connection_successful = 1;
+		if (g_start_message_sent == 1 && g_marco_blocking == 0 && g_screen_bounds_found == 1) {
+			g_last_data_sent_time = time(NULL);
+			x_coord = eventParams.X/g_screen_bound_x;
+			y_coord = eventParams.Y/g_screen_bound_y;
+			if (x_coord > 0 && x_coord < 1 && y_coord > 0 && y_coord < 1) {
+				_snprintf(send_message, 50, "%s gaze %.3f,%.3f",TOPIC_FILTER, x_coord, y_coord);
+				zmq_send(g_push_socket, send_message, strlen(send_message), 0);
+			}
+		}
 	} else {
 		send_zmq_message(ET_DATA_PACKET);
 	}
@@ -203,30 +245,30 @@ void TX_CALLCONVENTION HandleEvent(TX_CONSTHANDLE hAsyncData, TX_USERPARAM userP
 /*
 * Eyetracker start-up function. Sets up 0mq communication, sends eyetracker starting message and connects to Tobii Engine.
 */
-BOOL _on_start(char* subscription_filter, TX_CONTEXTHANDLE* hContext, TX_TICKET* hConnectionStateChangedTicket,
+BOOL _on_start(char* subscription_filter, TX_TICKET* hConnectionStateChangedTicket,
 			   TX_TICKET* hEventHandlerTicket) {
-	BOOL success;
+	BOOL success = 1;
 	// Create and initialize 0mq sockets
-	context = zmq_ctx_new();
-	subscription_socket = zmq_socket(context, ZMQ_SUB);
-	push_socket = zmq_socket(context,ZMQ_PUSH);
-	zmq_connect(subscription_socket, SOCKET_SUB);
-	zmq_connect(push_socket, SOCKET_PUSH);
-	zmq_setsockopt(subscription_socket, ZMQ_SUBSCRIBE, subscription_filter, strlen(subscription_filter));
+	g_zmqcontext = zmq_ctx_new();
+	g_subscription_socket = zmq_socket(g_zmqcontext, ZMQ_SUB);
+	g_push_socket = zmq_socket(g_zmqcontext,ZMQ_PUSH);
+	zmq_connect(g_subscription_socket, SOCKET_SUB);
+	zmq_connect(g_push_socket, SOCKET_PUSH);
+	zmq_setsockopt(g_subscription_socket, ZMQ_SUBSCRIBE, subscription_filter, strlen(subscription_filter));
 
 	send_zmq_message(ET_STARTING);
-	start_message_sent = 1;
+	g_start_message_sent = 1;
 
 	// initialize and enable the context that is our link to the EyeX Engine.
 	success = txInitializeEyeX(TX_EYEXCOMPONENTOVERRIDEFLAG_NONE, NULL, NULL, NULL, NULL) == TX_RESULT_OK;
-	success &= txCreateContext(hContext, TX_FALSE) == TX_RESULT_OK;
-	success &= InitializeGlobalInteractorSnapshot(*hContext);
-	success &= txRegisterConnectionStateChangedHandler(*hContext, hConnectionStateChangedTicket, OnEngineConnectionStateChanged, NULL) == TX_RESULT_OK;
-	success &= txRegisterEventHandler(*hContext, hEventHandlerTicket, HandleEvent, NULL) == TX_RESULT_OK;
+	success &= txCreateContext(&g_hContext, TX_FALSE) == TX_RESULT_OK;
+	success &= InitializeGlobalInteractorSnapshot(g_hContext);
+	success &= txRegisterConnectionStateChangedHandler(g_hContext, hConnectionStateChangedTicket, OnEngineConnectionStateChanged, NULL) == TX_RESULT_OK;
+	success &= txRegisterEventHandler(g_hContext, hEventHandlerTicket, HandleEvent, NULL) == TX_RESULT_OK;
 
 	// enables connection to EyeX Engine, connection alive
 	// until it is desabled or context is destroyed
-	success &= txEnableConnection(*hContext) == TX_RESULT_OK;
+	success &= txEnableConnection(g_hContext) == TX_RESULT_OK;
 
 	return success;
 }
@@ -236,33 +278,33 @@ BOOL _on_start(char* subscription_filter, TX_CONTEXTHANDLE* hContext, TX_TICKET*
 */
 void _on_marco() {
 	time_t current_time = time(NULL);
-	marco_blocking = 1;
-	if (connection_successful == 1 && (current_time - last_data_sent_time) < TIMEOUT_THRESHOLD) {
+	g_marco_blocking = 1;
+	if (g_connection_successful == 1 && (current_time - g_last_data_sent_time) < TIMEOUT_THRESHOLD) {
 		send_zmq_message(ET_MARCO);
 	}
 	else
-		connection_successful = 0;
-	marco_blocking = 0;
+		g_connection_successful = 0;
+	g_marco_blocking = 0;
 	return;
 }
 
 /*
 * After receiving stop command from server, close zmq sockets, destroy zmq context and disconnect from Tobii engine.
 */
-void _on_stop(TX_CONTEXTHANDLE* hContext) {
+void _on_stop(TX_CONTEXTHANDLE hContext) {
 	boolean success;
 	send_zmq_message(ET_STOPPING);
 	// zmq clean up
-	zmq_close(subscription_socket);
-	zmq_close(push_socket);
-	zmq_ctx_destroy(context);
+	zmq_close(g_subscription_socket);
+	zmq_close(g_push_socket);
+	zmq_ctx_destroy(g_zmqcontext);
 
 	// Tobii cleanup
 	// disable and delete the context.
-	txDisableConnection(*hContext);
+	txDisableConnection(hContext);
 	txReleaseObject(&g_hGlobalInteractorSnapshot);
-	success = txShutdownContext(*hContext, TX_CLEANUPTIMEOUT_DEFAULT, TX_FALSE) == TX_RESULT_OK;
-	success &= txReleaseContext(hContext) == TX_RESULT_OK;
+	success = txShutdownContext(hContext, TX_CLEANUPTIMEOUT_DEFAULT, TX_FALSE) == TX_RESULT_OK;
+	success &= txReleaseContext(&hContext) == TX_RESULT_OK;
 	success &= txUninitializeEyeX() == TX_RESULT_OK;
 
 	if (!success) {
@@ -276,7 +318,7 @@ void _on_stop(TX_CONTEXTHANDLE* hContext) {
 Command read_message() {
 	Command received = CONSOLE_NO_CMD;
 	char buffer[100];
-	zmq_recv(subscription_socket,buffer,100,ZMQ_DONTWAIT);
+	zmq_recv(g_subscription_socket,buffer,100,ZMQ_DONTWAIT);
 	if (strstr(buffer, "marco") != NULL) {
 		received = CONSOLE_MARCO;
 	}
@@ -292,7 +334,6 @@ Command read_message() {
 int main(int argc, char* argv[])
 {
 	// Eye tracker variables
-	TX_CONTEXTHANDLE hContext = TX_EMPTY_HANDLE;
 	TX_TICKET hConnectionStateChangedTicket = TX_INVALID_TICKET;
 	TX_TICKET hEventHandlerTicket = TX_INVALID_TICKET;
 	BOOL success;
@@ -301,18 +342,18 @@ int main(int argc, char* argv[])
 	int is_alive = 1;
 	Command received = CONSOLE_NO_CMD;
 
-	success = _on_start(subscription_filter, &hContext, &hConnectionStateChangedTicket, &hEventHandlerTicket);
+	success = _on_start(subscription_filter,&hConnectionStateChangedTicket,&hEventHandlerTicket);
 
 	if (!success) {
 		send_zmq_message(ET_INIT_FAILED);
-		_on_stop(&hContext);
+		_on_stop(g_hContext);
 	}
 
 	while (is_alive == 1) {
 		received = read_message();
 		if (received == CONSOLE_STOP) {
 			is_alive = 0;
-			_on_stop(&hContext);
+			_on_stop(g_hContext);
 		}
 		else if (received == CONSOLE_MARCO) {
 			_on_marco();

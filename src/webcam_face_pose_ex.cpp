d82034368fca1fb4d12a199ac4ce7aa2edf5a00c
maxlazarus <max.prokopenko@gmail.com>
2017-03-17 22:40:12 -0700

    Improved reliability of face.exe and conformation to wtfj protocol

--- src/webcam_face_pose_ex.cpp
+++ src/webcam_face_pose_ex.cpp
@@ -35,6 +35,7 @@
 #define _WINSOCKAPI_ 
 #include <iostream>
 #include <chrono>
+#include <thread>
 #include <dlib/opencv.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <dlib/image_processing/frontal_face_detector.h>
@@ -45,15 +46,77 @@
 
 using namespace dlib;
 
-int main() {
-    try {
-		zmq::context_t context(1);
-		zmq::socket_t sub(context, ZMQ_SUB);
-		zmq::socket_t push(context, ZMQ_PUSH);
+// Averages points given to it and stores in given x and y references
+template <class T>
+void average(float& x, float& y, const dlib::full_object_detection& points, std::initializer_list<T> list) {
+	x = 0;
+	y = 0;
+	int n = 0;
+	for (auto elem : list) {
+		x += static_cast<float>(points.part(elem).x());
+		y += static_cast<float>(points.part(elem).y());
+		n++;
+	}
+	x /= n;
+	y /= n;
+}
+
+
+class ZmqConnector {
+private:
+	zmq::socket_t sub;
+	zmq::socket_t push;
+	static zmq::context_t context;
+
+public:
+	ZmqConnector() : sub(context, ZMQ_SUB), push(context, ZMQ_PUSH) {
 		sub.connect("tcp://localhost:5556");
 		push.connect("tcp://localhost:5557");
 		const char *filter = "@face";
 		sub.setsockopt(ZMQ_SUBSCRIBE, filter, strlen(filter));
+		send("face starting");
+	}
+
+	virtual ~ZmqConnector() {
+		sub.close();
+		push.close();
+	}
+
+	void send(char* buffer) {
+		zmq::message_t sending(strlen(buffer));
+		memcpy(sending.data(), buffer, strlen(buffer));
+		push.send(sending);
+	}
+
+	bool receive() {
+		zmq::message_t received;
+		while (sub.recv(&received, ZMQ_DONTWAIT)) {
+			std::istringstream iss(static_cast<char*>(received.data()));
+			std::string topic;
+			std::string message;
+			iss >> topic >> message;
+			if (strstr(topic.c_str(), "@face")) {
+				if (strstr(message.c_str(), "stop")) {
+					char* exit_message = "face stopping";
+					send(exit_message);
+					return false;
+				}
+				else if (strstr(message.c_str(), "marco")) {
+					send("face polo");
+				}
+			}
+		}
+		return true;
+	}
+};
+
+zmq::context_t ZmqConnector::context = zmq::context_t(1);
+
+
+int main() {
+    try {
+
+		ZmqConnector zmq_connector;
 
         cv::VideoCapture cap(0);
         if (!cap.isOpened()) {
@@ -64,10 +127,22 @@ int main() {
         // Load face detection and pose estimation models.
         frontal_face_detector detector = get_frontal_face_detector();
         shape_predictor pose_model;
-        deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
+		try {
+			deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
+		} catch (serialization_error& e) {
+			try {
+				deserialize("../bin/face/shape_predictor_68_face_landmarks.dat") >> pose_model;
+			} catch (serialization_error& e) {
+				std::cout << "You need dlib's default face landmarking model file to run this example." << std::endl;
+				std::cout << "You can get it from the following URL: " << std::endl;
+				std::cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << std::endl;
+				std::cout << std::endl << e.what() << std::endl;
+			}
+		}
 
         // Grab and process frames until the main window is closed by the user.
 		bool alive(true);
+		int index(0);
 
 		while (alive) {
 			// Grab a frame
@@ -83,6 +158,10 @@ int main() {
 
 			// Detect faces
 			std::vector<rectangle> faces = detector(cimg);
+
+			// Don't hog the CPU, this limits updates to max 50Hz
+			std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
 			// Find the pose of each face
 			std::vector<full_object_detection> shapes;
 			for (unsigned long i = 0; i < faces.size(); ++i)
@@ -90,66 +169,42 @@ int main() {
 
 			if (faces.size() > 0) {
 				// Parts of face
-				// 1 - right side of jaw
-				// 3 - right side of jaw
-				// 5 - right side of jaw
-				// 19 - middle right eyebrow
-				// 21 - inner right eyebrow
-				// 22 - inner left eyebrow
-				// 24 - middle left eyebrow
-				// 33 - nose bottom centre
-				// 34 - nose bottom left
-				// 35 - nose left
-				// 37 - upper right eye 3/4
-				// 39 - inner right eye
-				// 41 - lower right eye 3/4
-				// 45 - outer left eye
-				// 49 - upper right lip
-				// 51 - middle of upper part of upper lip
-				// 53 - upper left lip
-				// 57 - lower part of lower lip
-				// 59 - lower right lip
-				// 62 - lower middle part of upper lip
-				// 67 - upper middle right part of lower lip
+				// 1 - 16 : jawline
+				// 17 - 21 : right eyebrow
+				// 22 - 26 : left eyebrow
+				// 27 - 35 : nose
+				// 36 - 41 : right eye
+				// 42 - 47 : left eye
+				// 48 - 54 : upper lip
+				// 54 - 60 : lower lip
+				// 61,62,63 : upper lip
+				// 48,60,54,64 : r-l corners (lower, upper?)
+				// 65,66,67 : lower lip
 
-				char buffer[] = "Default buffer message this is how long? I don't know!";
+				auto a = shapes[0];
+				// x and y for right brow, left brow, right eye, left eye, nose, upper mouth, lower mouth
+				float brx, bry, blx, bly, erx, ery, elx, ely, nx, ny, mux, muy, mlx, mly, jawx, jawy;
+				average(brx, bry, shapes[0], { 17, 18, 19, 20, 21 });
+				average(blx, bly, shapes[0], { 22, 23, 24, 25, 26 });
+				average(erx, ery, shapes[0], { 36, 37, 38, 39, 40, 41 });
+				average(elx, ely, shapes[0], { 42, 43, 44, 45, 46, 47 });
+				average(nx, ny, shapes[0], { 27, 28, 29, 30, 31, 32, 33, 34, 35 });
+				average(mux, muy, shapes[0], { 48, 49, 50, 51, 52, 53, 54 });
+				average(mlx, mly, shapes[0], { 54, 55, 56, 57, 58, 59, 60 });
+				average(jawx, jawy, shapes[0], { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 });
+
+				// Create the tranmission buffer and load it with a string in the form of:
+				// face position [comma seperated face point values]
+				char buffer[512];
 				sprintf(buffer,
-					"face %d,%d,%d,%d",
-					shapes[0].part(24).x(),
-					shapes[0].part(24).y(),
-					shapes[0].part(19).x(),
-					shapes[0].part(19).y(),
-					shapes[0].part(33).x(),
-					shapes[0].part(33).y(),
-					shapes[0].part(62).x(),
-					shapes[0].part(62).y(),
-					shapes[0].part(57).x(),
-					shapes[0].part(57).y()
+					"face position %f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f",
+					brx, bry, blx, bly, erx, ery, elx, ely, nx, ny, mux, muy, mlx, mly, jawx, jawy
 				);
-				zmq::message_t sending(strlen(buffer));
-				memcpy(sending.data(), buffer, strlen(buffer));
-				push.send(sending);
-
-				zmq::message_t received(13);
-				while (sub.recv(&received, ZMQ_DONTWAIT)) {
-					std::istringstream iss(static_cast<char*>(received.data()));
-					std::string topic;
-					std::string message;
-					iss >> topic >> message;
-					if (message.compare("cmd=quit")) {
-						alive = false;
-					}
-				}
+				
+				zmq_connector.send(buffer);
+				alive = zmq_connector.receive();
 			}
 		}
-		sub.close();
-		push.close();
-		context.close();
-    } catch(serialization_error& e) {
-        std::cout << "You need dlib's default face landmarking model file to run this example." << std::endl;
-        std::cout << "You can get it from the following URL: " << std::endl;
-        std::cout << "   http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << std::endl;
-        std::cout << std::endl << e.what() << std::endl;
     } catch(std::exception& e) {
         std::cout << e.what() << std::endl;
     }
